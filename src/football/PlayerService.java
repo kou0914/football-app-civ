7c2ac56ff06f492d43cd94fdd4ffcb313eeb2f4d
kou0914 <cclemon23152022@gmail.com>
2026-09-25 14:28:11 +0900

    編集

--- src/football/PlayerService.java
+++ src/football/PlayerService.java
@@ -220,9 +220,7 @@ public class PlayerService {
 					Player player = candidates.get(i);
 
 					System.out.println(
-							(i + 1) + ". " + player.getName() +
-									" OVR:" +
-									player.getOverall());
+							(i + 1) + ". " + player.getName() + " OVR:" + player.getOverall());
 				}
 
 				System.out.print("番号: ");
